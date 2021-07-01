6ef895f9a0e1b32112c7a3260f6b16df9aa1b702
Dimas014 <dimas05014@gmail.com>
2021-07-01 14:51:58 +0700

    first commit

---
+++ src/components/Contact/index.jsx
@@ -0,0 +1,27 @@
+import "./style.css";
+
+const About = () => {
+  return (
+    <div className="contact">
+      <h1>Contact me!</h1>
+      <iframe
+       src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d31736.64631685856!2d106.18073497699916!3d-6.119827561704687!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e41f504a4ce2e5b%3A0xd7fbb1e65b42cbd2!2sUniversitas%20Sultan%20Ageng%20Tirtayasa%20(Untirta)!5e0!3m2!1sid!2sid!4v1625121000422!5m2!1sid!2sid"   frameBorder="0"
+       frameBorder="0"
+        allowFullScreen=""
+        aria-hidden="false"
+        tabIndex="0"
+      ></iframe>
+
+      <h4>Phone</h4>
+      <p>0878216438xx</p>
+
+      <h4>Email</h4>
+      <p>dimasxxxxx@gmail.com</p>
+
+      <h4>Address</h4>
+      <p>Jln. Raya Jakarta Km 4.5</p>
+    </div>
+  );
+};
+
+export default About;
